8a298043c139251d91274449d03f76fc7f37a738
toowan <mail@toowan.me>
2017-01-30 21:15:03 -0800

    updated css

--- js/javascript.js
+++ js/javascript.js
@@ -10,8 +10,18 @@ $(document).ready(function() {
   })
 
 
-  // When search icon is clicked, run wikiCallback
-  $('#search').click(wikiCallback);
+  // When search is clicked, move search bar up and show results
+  $("#search").click(function(e) {
+      e.preventDefault();
+      wikiCallback(e);
+      $('#search-text').val(""); //clears value in text box
+      $('#search-bar').animate({top: "3%"}, 300); // moves search bar up
+      $('#content').show().animate({
+        opacity: 1,
+        marginTop: "130px"
+      }, 800); 
+  });
+
 
   // When enter key is pressed, run wikiCallback
   $('#search-text').keypress(function(e) {
@@ -23,47 +33,39 @@ $(document).ready(function() {
 
   // Callback function
   function wikiCallback(e) {
-    console.log('wikiCallback');
     // Gets search input
     var searchText = $('#search-text').val(); 
     //API url with searchTerm
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchText + "&format=json&callback=?";
 
-    // Wikipedia Ajax call
-    $.ajax({
-      type: "GET",
-      url: url,
-      async: false,
-      dataType: "json",
-      success: function(data){
-        // console.log(data[1][0]); - heading/title of article
-        // console.log(data[2][0]); - description 
-        // console.log(data[3][0]); - wikipedia link for article
-        $('#output').html(''); // Clears previous results before displaying current ones
-        for (var i = 0; i < data[1].length; i++){
-          $('#output').prepend("<li><a href='"+data[3][i]+"' target='_blank'>"+data[1][i] +"</a><p>"+data[2][i]+"</p></li>");
+    if (searchText == '') {
+      $('#output').html('No results found.');
+    } else {
+      // Wikipedia Ajax call
+      $.ajax({
+        type: "GET",
+        url: url,
+        async: false,
+        dataType: "json",
+        success: function(data){
+          // console.log(data[1][0]); - heading/title of article
+          // console.log(data[2][0]); - description 
+          // console.log(data[3][0]); - wikipedia link for article
+          $('#output').html(''); // Clears previous results before displaying current ones
+          for (var i = 0; i < data[1].length; i++){
+            $('#output').prepend("<li><a href='"+data[3][i]+"' target='_blank'>"+data[1][i] +"</a><p>"+data[2][i]+"</p></li>");
+          }
+        },
+        error: function(errorMessage){
+          alert("Error");
         }
-      },
-      error: function(errorMessage){
-        alert("Error");
-      }
 
-    });
+      });
+    };  
   }
 
   // Hide results by default 
   $('#content').hide();
-
-  // When search is clicked, move search bar up and show results
-  $("#search").click(function (e) {
-      e.preventDefault();
-      $('#search-text').val(""); //clears value in text box
-      $('#search-bar').animate({top: "3%"}, 300); // moves search bar up
-      $('#content').show().animate({
-        opacity: 1,
-        marginTop: "150px"
-      }, 800); 
-  });
 
 
   // Back to top
